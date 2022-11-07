e896ecbc5a27fe925a74871894672512cd9ed130
alvaro <alvargmez@gmail.com>
2022-11-07 23:47:59 +0100

    ejercicio_1

---
+++ src/primera_parte/prueba_ejercicio.java
@@ -0,0 +1,13 @@
+package primera_parte;
+
+public class prueba_ejercicio {
+
+    public static void main(String[] args) {
+
+        suma_p prueba = new suma_p(1,2,3);
+
+        System.out.println("La sumna de mis números es: " + prueba.mi_suma());
+
+    }
+
+}
---
+++ src/primera_parte/suma_p.java
@@ -0,0 +1,20 @@
+package primera_parte;
+
+public class suma_p {
+    
+    private int suma;
+
+    public suma_p (int a, int b, int c){
+
+        suma = a+b+c;
+
+    }
+
+    //getter
+    public int mi_suma(){
+
+        return suma;
+
+    }
+
+}
